5ded27f1bfe81c64d9c181489ef9dbee0e7b2530
Kwon <sanghyukkwon90@gmail.com>
2023-03-24 13:12:55 -0400

    hopes

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from report_generator.views import generate_report_view
+from views import generate_report_view
 
 urlpatterns = [
     path('generate_report/', generate_report_view, name='generate_report'),
